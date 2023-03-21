952dd5f5f20c3edc8920ce7508c99c125d821c2d
Vetle Ledaal <vetle.ledaal@gmail.com>
2023-03-21 23:26:50 +0100

    update

--- find_best.py
+++ find_best.py
@@ -6,8 +6,8 @@ import tempfile
 import multiprocessing
 
 
-def run(log_dir, dataset, T, s, feature_set):
-    fname = f"{dataset}_max-vocab=15000_pre=v1_{'_'.join(feature_set)}_T={T}_s={s}"
+def run(log_dir, dataset, T, s, feature_set, max_vocab, preprocessor):
+    fname = f"{dataset}_max-vocab={max_vocab}_pre={preprocessor}_{'_'.join(feature_set)}_T={T}_s={s}"
     fnamelog = f'{fname}.log'
     fnametmp = f'{fname}.tmp.'
     log_file = os.path.join(log_dir, fnamelog)
@@ -18,7 +18,8 @@ def run(log_dir, dataset, T, s, feature_set):
     with tempfile.NamedTemporaryFile(delete=False, dir=log_dir, prefix=fnametmp) as temp_file:
         cmd = ['python', '-u', 'pipeline.py']
         cmd += ['--dataset', dataset]
-        cmd += ['--max-vocab', '15000']
+        cmd += ['--max-vocab', max_vocab]
+        cmd += ['--preprocessor', preprocessor]
         cmd += ['--T', T]
         cmd += ['--s', s]
         cmd += ['--epochs', '150']
@@ -30,6 +31,7 @@ def run(log_dir, dataset, T, s, feature_set):
         for line in iter(p.stdout.readline, b''):
             if args.monitor:
                 print(line.decode('utf-8'), end='', flush=True)
+
             temp_file.write(line)
             temp_file.flush()
 
@@ -43,27 +45,34 @@ def main(args):
     features = ('text', 'domain')
     ts = ('50', '150', '200', '250')
     ss = ('5', '10', '15')
+    max_vocabs = ('10000',)
+    preprocessors = ('v2',)
 
     log_dir = "logs"
     os.makedirs(log_dir, exist_ok=True)
 
-    batch_size = 8
+    combinations = [
+        (subset, t, s, max_vocab, preprocessor) for i in range(len(features)+1)
+        for subset in itertools.combinations(features, i)
+        for t, s, max_vocab, preprocessor in itertools.product(ts, ss, max_vocabs, preprocessors)
+    ]
+
+    batch_size = 2
 
     with multiprocessing.Pool(batch_size) as p:
         for dataset in datasets:
-            for k in range(1, len(features)+1):
-                for feature_set in itertools.combinations(features, k):
-                    for l in range(1, len(ts)+1):
-                        for T in itertools.combinations(ts, l):
-                            for m in range(1, len(ss)+1):
-                                for s in itertools.combinations(ss, m):
-                                    p.apply_async(run, kwds={
-                                        'log_dir': log_dir,
-                                        'dataset': dataset,
-                                        'T': T[0],
-                                        's': s[0],
-                                        'feature_set': feature_set,
-                                    })
+            for feature_set, T, s, max_vocab, preprocessor in combinations:
+                if len(feature_set) > 0:
+                    p.apply_async(run, kwds={
+                        'log_dir': log_dir,
+                        'dataset': dataset,
+                        'T': T,
+                        's': s,
+                        'max_vocab': max_vocab,
+                        'preprocessor': preprocessor,
+                        'feature_set': feature_set,
+                    })
+
         p.close()
         p.join()
 
--- pipeline.py
+++ pipeline.py
@@ -2,27 +2,33 @@ import argparse
 import glob
 import logging
 import os
+import string
 from collections import Counter
 from time import time
-from typing import Dict, List, NamedTuple, Optional, Tuple
-
+from typing import Dict, List, NamedTuple, Optional, Tuple, Union
+import sys
+import nltk
 import numpy as np
 import pandas as pd
 import regex
 import tldextract
+from nltk.stem import WordNetLemmatizer
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tmu.models.classification.vanilla_classifier import TMClassifier
 
+nltk.download('wordnet')
+nltk.download('stopwords')
+
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # above keras
 
 
 class TrainTestSet(NamedTuple):
-    train_X: np.ndarray
-    train_Y: np.ndarray
-    test_X: np.ndarray
-    test_Y: np.ndarray
+    train_x: np.ndarray
+    train_y: np.ndarray
+    test_x: np.ndarray
+    test_y: np.ndarray
 
 noisy_loggers = [
     'tmu.models.classification.vanilla_classifier',
@@ -31,53 +37,84 @@ noisy_loggers = [
 for logger in noisy_loggers:
     logging.getLogger(logger).setLevel(logging.INFO)
 
+
 # Precompile regular expressions
 # Based on https://github.com/yoonkim/CNN_sentence/blob/23e0e1f7355705bb083043fda05c031b15acb38c/process_data.py#L97
-RE_NOT_ALNUM = regex.compile(r"[^A-Za-z0-9(),!?'`]")
-RE_CONTRACTION = regex.compile(r"'([std]|ve|re|ll)")
-RE_PUNCTUATION = regex.compile(r"([(),!?'`])")
-RE_WHITESPACE = regex.compile(r"\s{2,}")
+RE_NOT_ALNUM = regex.compile(r'[^A-Za-z0-9(),!?\'`]')
+RE_CONTRACTION = regex.compile(r'\'([std]|ve|re|ll)')
+RE_PUNCTUATION = regex.compile(r'([(),!?\'`])')
+RE_WHITESPACE = regex.compile(r'\s{2,}')
 
-
-def clean_str(text):
-    text = RE_NOT_ALNUM.sub(" ", text)
-    text = RE_CONTRACTION.sub(" '\\1", text)
-    text = RE_PUNCTUATION.sub(" \\1 ", text)
-    # TODO: remove stop words here
-    text = RE_WHITESPACE.sub(" ", text)
+def pre_v1(text):
+    text = RE_NOT_ALNUM.sub(' ', text)
+    text = RE_CONTRACTION.sub(' \'\\1', text)
+    text = RE_PUNCTUATION.sub(' \\1 ', text)
+    text = RE_WHITESPACE.sub(' ', text)
     return text.strip().lower().split()
 
 
-def generate_word_freq(*series: pd.Series, max_n: Optional[int] = None) -> List[Tuple[str, int]]:
-    word_counts = Counter()
+pre_v2_lemmatizer = WordNetLemmatizer()
+pre_v2_stop_words = set(nltk.corpus.stopwords.words('english'))
+pre_v2_translator = str.maketrans('', '', string.punctuation.replace('\'', ''))
+pre_v2_cache = {}
+
+for word in pre_v2_stop_words:
+    pre_v2_cache[word] = None
+
+def pre_v2_word(word):
+    word = word.lower()
+    if word in pre_v2_cache:
+        return pre_v2_cache[word]
+    result = word.translate(pre_v2_translator).strip('\'')
+    result = pre_v2_lemmatizer.lemmatize(result)
+    if len(result) < 3 or result.isdigit():
+        return None
+    pre_v2_cache[word] = result
+    return result
+
+def pre_v2(text):
+    words = text.split()
+    return list(filter(None, map(pre_v2_word, words)))
+
+
+def pre_tweet_ids(text):
+    return [int(num_str) for num_str in text.split()]
+
+
+def pre_domain(text):
+    try:
+        return tldextract.extract(text).domain
+    except:
+        return ''
+
+
+def token_counter(*series: pd.Series, clean_func, max_n: Optional[int] = None) -> List[Tuple[str, int]]:
+    counter = Counter()
     for s in series:
         for row in s:
-            tokens = clean_str(row)
-            word_counts.update(tokens)
-    return word_counts.most_common(max_n)
+            tokens = clean_func(row)
+            counter.update(tokens)
+    return [x for x in counter.most_common(max_n) if x[1] >= 3]
+
+def create_token_index_map(*series: pd.Series, clean_func, max_n: Optional[int] = None) -> Dict[str, int]:
+    token_counts = token_counter(*series, clean_func=clean_func, max_n=max_n)
+    return {token_count: idx for idx, (token_count, _) in enumerate(token_counts)}
 
 
-def generate_word_to_idx(*series: pd.Series, max_n: Optional[int] = None) -> Dict[str, int]:
-    word_freq = generate_word_freq(*series, max_n=max_n)
-    return {word: idx for idx, (word, _) in enumerate(word_freq)}
-
-
-def bag_of_words(sentences: pd.Series, word_to_idx: Dict[str, int]):
-    sentences = sentences.apply(clean_str)
-    # lil_matrix not supported, is flattened in TMU
-    # Using np.uint32 as it's the same in TMU
-    matrix = np.zeros((len(sentences), len(word_to_idx)), dtype=np.uint32)
-    for i, l in enumerate(sentences):
-        for w in l:
-            if w in word_to_idx:
-                matrix[i, word_to_idx[w]] = 1
+def binary_bag_of_words(token_series: pd.Series, token_index_map: Dict[Union[str, int], int], clean_func):
+    token_series = token_series.apply(clean_func)
+    matrix = np.zeros((len(token_series), len(token_index_map)), dtype=np.uint32)
+    for i, tokens in enumerate(token_series):
+        for token in tokens:
+            if token in token_index_map:
+                matrix[i, token_index_map[token]] = 1
     return matrix
 
 
 def train(a: TrainTestSet, metrics: Optional[List[str]] = None):
     # Sanity check
-    assert a.train_X.shape[0] == a.train_Y.shape[0]
-    assert a.test_X.shape[0] == a.test_Y.shape[0]
+    assert a.train_x.shape[0] == a.train_y.shape[0]
+    assert a.test_x.shape[0] == a.test_y.shape[0]
 
     tm = TMClassifier(
         number_of_clauses=args.num_clauses,
@@ -92,39 +129,38 @@ def train(a: TrainTestSet, metrics: Optional[List[str]] = None):
     print(f'[*] Training for {args.epochs} epochs...')
     for i in range(args.epochs):
         time_before = time()
-        tm.fit(a.train_X, a.train_Y) # TODO: seed. Uses np.random.shuffle() internally
+        tm.fit(a.train_x, a.train_y) # TODO: seed. Uses np.random.shuffle() internally
         time_fit = time() - time_before
 
         if metrics is not None:
             time_before = time()
-            train_Y_predict = tm.predict(a.train_X)
+            train_y_predict = tm.predict(a.train_x)
             time_train_predict = time() - time_before
-            
+
             time_before = time()
-            test_Y_predict = tm.predict(a.test_X)
+            test_y_predict = tm.predict(a.test_x)
             time_test_predict = time() - time_before
 
             print(f'Epoch {str(i+1).rjust(3)}: Train%/Test%', end='')
             if 'acc' in metrics:
-                accuracy_train = accuracy_score(a.train_Y, train_Y_predict)
-                accuracy_test = accuracy_score(a.test_Y, test_Y_predict)
-                print(f" | Accuracy: {accuracy_train:.2%}/{accuracy_test:.2%}", end='')
+                accuracy_train = accuracy_score(a.train_y, train_y_predict)
+                accuracy_test = accuracy_score(a.test_y, test_y_predict)
+                print(f' | Accuracy: {accuracy_train:.2%}/{accuracy_test:.2%}', end='')
             if 'prec' in metrics:
-                precision_train = precision_score(a.train_Y, train_Y_predict, average='macro')
-                precision_test = precision_score(a.test_Y, test_Y_predict, average='macro')
-                print(f" | Precision: {precision_train:.2%}/{precision_test:.2%}", end='')
+                precision_train = precision_score(a.train_y, train_y_predict, average='macro', zero_division=0)
+                precision_test = precision_score(a.test_y, test_y_predict, average='macro', zero_division=0)
+                print(f' | Precision: {precision_train:.2%}/{precision_test:.2%}', end='')
             if 'rec' in metrics:
-                recall_train = recall_score(a.train_Y, train_Y_predict, average='macro')
-                recall_test = recall_score(a.test_Y, test_Y_predict, average='macro')
-                print(f" | Recall: {recall_train:.2%}/{recall_test:.2%}", end='')
+                recall_train = recall_score(a.train_y, train_y_predict, average='macro')
+                recall_test = recall_score(a.test_y, test_y_predict, average='macro')
+                print(f' | Recall: {recall_train:.2%}/{recall_test:.2%}', end='')
             if 'f1' in metrics:
-                f1_train = f1_score(a.train_Y, train_Y_predict, average='macro')
-                f1_test = f1_score(a.test_Y, test_Y_predict, average='macro')
-                print(f" | F1: {f1_train:.2%}/{f1_test:.2%}", end='')
+                f1_train = f1_score(a.train_y, train_y_predict, average='macro')
+                f1_test = f1_score(a.test_y, test_y_predict, average='macro')
+                print(f' | F1: {f1_train:.2%}/{f1_test:.2%}', end='')
 
         time_total = time_fit + time_train_predict + time_test_predict
-        print(f' | Time: fit={time_fit:03.0f}s, train={time_train_predict:03.0f}s, test={time_test_predict:03.0f}s, total={time_total:03.0f}s', end='')
-        print('') # explicit newline
+        print(f' | Time: fit={time_fit:03.0f}s, train={time_train_predict:03.0f}s, test={time_test_predict:03.0f}s, total={time_total:03.0f}s')
 
 
 '''
@@ -150,7 +186,7 @@ def load_FakeNewsNet(subset='*'):
 def load_FakeCovid():
     df = pd.read_csv('FakeCovid_July2020.csv')
     df = df.rename(columns={'source_title': 'text', 'article_source': 'url', 'class': 'label'})
-    
+
     # 10 is a magic number
     # TODO: change to min x occurrences
     # why limit?: labels should occur in both train and test (check if sklearn guarantees this)
@@ -158,62 +194,57 @@ def load_FakeCovid():
     df = df[df['label'].isin([label[0] for label in labels])]
     return df
 
-def extract_domain(url):
-    try:
-        return tldextract.extract(url).domain
-    except:
-        return ''
+def encode_df(train_df: pd.DataFrame, test_df: pd.DataFrame,
+    clean_func, features: List[str], max_vocab: int,
+    max_domain: int, max_tweet: int) -> TrainTestSet:
 
+    params = {
+        'text': {'max_n': max_vocab, 'encoder': clean_func, 'key': 'text'},
+        'url': {'max_n': max_domain, 'encoder': pre_domain, 'key': 'domain'},
+        'tweet_ids': {'max_n': max_tweet, 'encoder': pre_tweet_ids, 'key': 'tweet'}
+    }
 
-def encode_df(train_df: pd.DataFrame, test_df: pd.DataFrame) -> TrainTestSet:
-    train_X_text = None
-    test_X_text = None
-    train_X_domain = None
-    test_X_domain = None
-    train_X_tweet_ids = None
-    test_X_tweet_ids = None
+    if 'all' not in features:
+        params = {k: v for k, v in params.items() if v['key'] in features}
 
-    if any(f in args.feature for f in ('all', 'text')):
-        print('[*] -> text...')
-        word_to_idx_text = generate_word_to_idx(train_df['text'], max_n=args.max_vocab)
-        train_X_text = bag_of_words(train_df['text'], word_to_idx_text)
-        test_X_text = bag_of_words(test_df['text'], word_to_idx_text)
+    train_xs = []
+    test_xs = []
+    for column, values in params.items():
+        max_n, encoder, name = values['max_n'], values['encoder'], values['key']
 
-    if any(f in args.feature for f in ('all', 'domain')):
-        if 'url' in train_df.columns and 'url' in test_df.columns:
-            print('[*] -> domain...')
-            train_df['domain'] = train_df['url'].apply(extract_domain)
-            test_df['domain'] = test_df['url'].apply(extract_domain)
-            word_to_idx_domain = generate_word_to_idx(train_df['domain'], max_n=args.max_domain)
-            train_X_domain = bag_of_words(train_df['domain'], word_to_idx_domain)
-            test_X_domain = bag_of_words(test_df['domain'], word_to_idx_domain)
+        if all(column in df.columns for df in (train_df, test_df)):
+            print(f'[*] Encoding feature: {name}...', end='')
 
-    if any(f in args.feature for f in ('all', 'tweet')):
-        if 'tweet_ids' in train_df.columns and 'tweet_ids' in test_df.columns:
-            print('[*] -> tweet_ids...')
-            word_to_idx_tweet_ids = generate_word_to_idx(train_df['tweet_ids'], max_n=args.max_tweet)
-            train_X_tweet_ids = bag_of_words(train_df['tweet_ids'], word_to_idx_tweet_ids)
-            test_X_tweet_ids = bag_of_words(test_df['tweet_ids'], word_to_idx_tweet_ids)
-    
-    print('[*] Encoding complete!') # more or less
+            token_index_map = create_token_index_map(train_df[column], clean_func=encoder, max_n=max_n)
+            train_x = binary_bag_of_words(train_df[column], token_index_map, encoder)
+            test_x = binary_bag_of_words(test_df[column], token_index_map, encoder)
+            print(f' {len(token_index_map)}')
+
+            train_xs.append(train_x)
+            test_xs.append(test_x)
+
+        elif name in features:
+            print(f'[!] Fatal error, feature {name} was explicitly specified, but not found')
+            sys.exit(1)
+
+    train_x = np.concatenate(train_xs, axis=1)
+    test_x = np.concatenate(test_xs, axis=1)
+
     le = LabelEncoder()
+    train_y = le.fit_transform(train_df['label'])
+    test_y = le.transform(test_df['label'])
 
-    train_X_sub = [x for x in (train_X_text, train_X_domain, train_X_tweet_ids) if x is not None]
-    train_X = np.concatenate(train_X_sub, axis=1)
-    train_Y = le.fit_transform(train_df['label'])
-
-    test_X_sub = [x for x in (test_X_text, test_X_domain, test_X_tweet_ids) if x is not None]
-    test_X = np.concatenate(test_X_sub, axis=1)
-    test_Y = le.fit_transform(test_df['label'])
-
-    return TrainTestSet(train_X, train_Y, test_X, test_Y)
+    return TrainTestSet(train_x, train_y, test_x, test_y)
 
 
 def fix_malformed(df: pd.DataFrame) -> pd.DataFrame:
     if args.malformed == 'fix':
-        if 'text' in df.columns: df['text'] = df['text'].fillna('')
-        if 'url' in df.columns: df['url'] = df['url'].fillna('')
-        if 'tweet_ids' in df.columns: df['tweet_ids'] = df['tweet_ids'].fillna('')
+        if 'text' in df.columns:
+            df['text'] = df['text'].fillna('')
+        if 'url' in df.columns:
+            df['url'] = df['url'].fillna('')
+        if 'tweet_ids' in df.columns:
+            df['tweet_ids'] = df['tweet_ids'].fillna('')
     elif args.malformed == 'drop':
         cols_to_drop = [col for col in ('text', 'domain', 'tweet_ids') if col in df.columns]
         if len(cols_to_drop) > 0:
@@ -224,22 +255,30 @@ def fix_malformed(df: pd.DataFrame) -> pd.DataFrame:
 def main(args):
     np.random.seed(args.seed)
 
-    print("[*] Load dataset...")
+    print('[*] Load dataset...')
     if args.dataset.startswith('FakeNewsNet'):
         subset = args.dataset.split('-')[-1] if '-' in args.dataset else '*'
         df = load_FakeNewsNet(subset=subset)
     elif args.dataset == 'FakeCovid':
         df = load_FakeCovid()
 
-    
     print('[*] Fix malformed data...')
     df = fix_malformed(df)
 
-    print("[*] Split...")
+    print('[*] Split...')
     train_df, test_df = train_test_split(df, test_size=args.test_size, random_state=args.seed) # TODO: reuse seed state
-    
-    print("[*] Encoding...")
-    train_test_set = encode_df(train_df, test_df)
+
+    print('[*] Encoding...')
+    clean_func = pre_v1 if args.preprocessor == 'v1' else pre_v2
+    train_test_set = encode_df(
+        train_df=train_df,
+        test_df=test_df,
+        clean_func=clean_func,
+        features=args.feature,
+        max_vocab=args.max_vocab,
+        max_domain=args.max_domain,
+        max_tweet=args.max_tweet
+    )
 
     print('='*30)
     print(f'Using {args.device} for training')
@@ -249,29 +288,30 @@ def main(args):
     print(f'Feature(s): {", ".join(args.feature)}')
     print(f'max-vocab={args.max_vocab}, max-domain={args.max_domain}, max-tweet={args.max_tweet}')
     print('='*20)
-    print(f'Train size: {train_test_set.train_X.shape[0]}')
-    print(f'Train classes: {len(set(train_test_set.train_Y))}')
-    print(f'Test size: {train_test_set.test_Y.shape[0]}')
-    print(f'Test classes: {len(set(train_test_set.test_Y))}')
-    print(f'Total features: {train_test_set.train_X.shape[1]}')
+    print(f'Train size: {train_test_set.train_x.shape[0]}')
+    print(f'Train classes: {len(set(train_test_set.train_y))}')
+    print(f'Test size: {train_test_set.test_y.shape[0]}')
+    print(f'Test classes: {len(set(train_test_set.test_y))}')
+    print(f'Total features: {train_test_set.train_x.shape[1]}')
     print('='*30)
     train(train_test_set, metrics=('acc', 'prec', 'rec', 'f1'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num-clauses', default=5000, type=int) # must be >5000
-    parser.add_argument('--T', default=100, type=int) # step 50
-    parser.add_argument('--s', default=10.0, type=float) # 5 10 15
-    parser.add_argument('--epochs', default=100, type=int) # 100 150
-    parser.add_argument('--device', default='GPU', type=str)
-    parser.add_argument('--seed', default=42, type=int)
-    parser.add_argument('--dataset', choices=('FakeNewsNet', 'FakeCovid', 'FakeNewsNet-gossipcop', 'FakeNewsNet-politifact'), default='FakeNewsNet')
-    parser.add_argument('--feature', choices=('all', 'text', 'domain', 'tweet'), default='all', nargs='+')
-    parser.add_argument('--test-size', default=0.2, type=float)
-    parser.add_argument('--malformed', choices=('fix', 'drop'), default='fix')
-    parser.add_argument('--max-vocab', default=3000, type=int) # 15000 (if too slow 10000)
-    parser.add_argument('--max-domain', default=500, type=int)
-    parser.add_argument('--max-tweet', default=500, type=int)
+    parser.add_argument('-n',  '--num-clauses', default=5000, type=int) # must be >5000
+    parser.add_argument('-t',  '--T', default=100, type=int) # step 50
+    parser.add_argument('-s',  '--s', default=10.0, type=float) # 5 10 15
+    parser.add_argument('-e',  '--epochs', default=100, type=int) # 100 150
+    parser.add_argument('-d',  '--device', default='GPU', type=str)
+    parser.add_argument('-sd', '--seed', default=42, type=int)
+    parser.add_argument('-ds', '--dataset', choices=('FakeNewsNet', 'FakeCovid', 'FakeNewsNet-gossipcop', 'FakeNewsNet-politifact'), required=True)
+    parser.add_argument('-f',  '--feature', choices=('all', 'text', 'domain', 'tweet'), default=('all',), nargs='+')
+    parser.add_argument('-ts', '--test-size', default=0.2, type=float)
+    parser.add_argument('-m',  '--malformed', choices=('fix', 'drop'), default='fix')
+    parser.add_argument('-mv', '--max-vocab', default=3000, type=int) # 15000 (if too slow 10000)
+    parser.add_argument('-md', '--max-domain', default=500, type=int)
+    parser.add_argument('-mt', '--max-tweet', default=500, type=int)
+    parser.add_argument('-p',  '--preprocessor', choices=('v1', 'v2'), default='v1')
     args = parser.parse_args()
     main(args)
